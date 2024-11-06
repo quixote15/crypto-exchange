e7aabe9372d42188144ee677396a9126db3f354c
quixote15 <santos2tiago@gmail.com>
2024-11-06 11:04:21 -0300

    chore: finish cancel order

--- main.go
+++ main.go
@@ -1,11 +1,173 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
 	"github.com/labstack/echo"
+	"github.com/quixote15/crypto-exchange/orderbook"
 )
 
 func main() {
 	e := echo.New()
+	ex := NewExchange()
+
+	e.GET("/book/:market", ex.handleGetBook)
+	e.POST("/order", ex.handlePlaceOrder)
+	e.DELETE("/order/:id", ex.cancelOrder)
 
 	e.Start(":3000")
+}
+
+type OrderType string
+
+type Market string
+
+const (
+	MarketETH Market = "ETH"
+)
+
+type Exchange struct {
+	orderbooks map[Market]*orderbook.OrderBook
+}
+
+func NewExchange() *Exchange {
+	orderbooks := make(map[Market]*orderbook.OrderBook)
+	orderbooks[MarketETH] = orderbook.NewOrderBook()
+	return &Exchange{
+		orderbooks: orderbooks,
+	}
+}
+
+type PlaceOrderRequest struct {
+	Type OrderType
+
+	Bid    bool
+	Size   float64
+	Price  float64
+	Market Market
+}
+
+type Order struct {
+	ID        int64
+	Price     float64
+	Size      float64
+	Bid       bool
+	Timestamp int64
+}
+
+type OrderBookData struct {
+	Asks []*Order
+	Bids []*Order
+}
+
+const (
+	MarketOrder OrderType = "MARKET"
+	LimitOrder  OrderType = "LIMIT"
+)
+
+func (ex *Exchange) handleGetBook(c echo.Context) error {
+	market := Market(c.Param("market"))
+
+	ob, ok := ex.orderbooks[market]
+
+	if !ok {
+		return c.JSON(404, map[string]any{"msg": "market not found"})
+	}
+
+	orderBookData := OrderBookData{
+		Asks: []*Order{},
+		Bids: []*Order{},
+	}
+
+	for _, limit := range ob.Asks() {
+		for _, order := range limit.Orders {
+			o := Order{
+				ID:        order.ID,
+				Price:     limit.Price,
+				Size:      order.Size,
+				Bid:       order.Bid,
+				Timestamp: order.Timestamp,
+			}
+			orderBookData.Asks = append(orderBookData.Asks, &o)
+		}
+
+	}
+
+	for _, limit := range ob.Bids() {
+		for _, order := range limit.Orders {
+			o := Order{
+				ID:        order.ID,
+				Price:     limit.Price,
+				Size:      order.Size,
+				Bid:       order.Bid,
+				Timestamp: order.Timestamp,
+			}
+			orderBookData.Bids = append(orderBookData.Bids, &o)
+		}
+
+	}
+	return c.JSON(http.StatusOK, orderBookData)
+}
+
+func (ex *Exchange) handlePlaceOrder(c echo.Context) error {
+	var placeOrderData PlaceOrderRequest
+	if err := json.NewDecoder(c.Request().Body).Decode(&placeOrderData); err != nil {
+		return err
+	}
+
+	market := Market(placeOrderData.Market)
+	ob := ex.orderbooks[market]
+	order := orderbook.NewOrder(placeOrderData.Bid, placeOrderData.Size)
+
+	if placeOrderData.Type == LimitOrder {
+		ob.PlaceLimitOrder(placeOrderData.Price, order)
+		return c.JSON(200, map[string]any{"msg": "order placed"})
+	}
+	if placeOrderData.Type == MarketOrder {
+		ob.PlaceMartketOrder(order)
+		return c.JSON(200, map[string]any{"msg": "order placed"})
+	}
+
+	return nil
+}
+
+func (ex *Exchange) cancelOrder(c echo.Context) error {
+	idStr := c.Param("id")
+	id, _ := strconv.Atoi(idStr)
+
+	ob := ex.orderbooks[MarketETH]
+
+	orderCanceled := false
+
+	for _, limit := range ob.Asks() {
+		for _, order := range limit.Orders {
+			if order.ID == int64(id) {
+				ob.CancelOrder(order)
+				orderCanceled = true
+				break
+			}
+
+			if orderCanceled {
+				return c.JSON(200, map[string]any{"msg": "order canceled"})
+			}
+		}
+	}
+
+	for _, limit := range ob.Bids() {
+		for _, order := range limit.Orders {
+			if order.ID == int64(id) {
+				ob.CancelOrder(order)
+				orderCanceled = true
+				break
+			}
+
+			if orderCanceled {
+				return c.JSON(200, map[string]any{"msg": "order canceled"})
+			}
+		}
+	}
+
+	return nil
 }
--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -2,6 +2,7 @@ package orderbook
 
 import (
 	"fmt"
+	"math/rand"
 	"sort"
 	"time"
 )
@@ -14,6 +15,7 @@ type Match struct {
 }
 
 type Order struct {
+	ID        int64
 	Size      float64
 	Bid       bool
 	Limit     *Limit
@@ -79,6 +81,7 @@ func NewLimit(price float64) *Limit {
 
 func NewOrder(bid bool, size float64) *Order {
 	return &Order{
+		ID:        int64(rand.Intn(100000)),
 		Size:      size,
 		Bid:       bid,
 		Timestamp: time.Now().UnixNano(),
